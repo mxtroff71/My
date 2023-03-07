d86ba96cc3b568cc23b80963365e9a32de1989ef
mxtroff71 <126468539+mxtroff71@users.noreply.github.com>
2023-03-07 09:44:42 +0300

    line changed

--- Triangle.py
+++ Triangle.py
@@ -1,10 +1,11 @@
 def triangle():
   stars = 1
-  for line in range(6):
-    print(' ' * (6 - line - 1), end = '')
+  for lines in range(6):
+    print(' ' * (6 - lines - 1), end = '')
     print('*' * stars)
     stars += 2
 
 
+triangle()
 triangle()
 triangle()
